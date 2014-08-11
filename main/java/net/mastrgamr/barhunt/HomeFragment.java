3a58c3208bdc5cd98fd6463bf4794fdbc9d06965
Stuart Smith <themastrgamr@gmail.com>
2014-08-10 22:59:40 -0400

    first commit

---
+++ main/java/net/mastrgamr/barhunt/HomeFragment.java
@@ -0,0 +1,56 @@
+package net.mastrgamr.barhunt;
+
+/**
+ * Created by mastrgamr on 8/10/14.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class HomeFragment extends Fragment {
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private TextView barInfoTxt;
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static HomeFragment newInstance(int sectionNumber) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        barInfoTxt = (TextView) rootView.findViewById(R.id.barInfoTxt);
+        barInfoTxt.setText("Hol\' up, test!");
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((Main) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
